ff6e541c752583a326893e35a538362bd00d2b2e
IMeIman <146995653+IMeIman@users.noreply.github.com>
2026-06-10 12:19:16 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    site: 'https://imeiman.github.io',
+    base: '/newgetup',
     plugins: [tailwindcss()],
   },
 });
